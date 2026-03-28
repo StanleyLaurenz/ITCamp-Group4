31febbf97616514207a33bf3dba5dda9311a51a5
Monica-Cheng <moicacheng03@gmail.com>
2026-03-28 22:56:58 +0800

    updates

--- apps/web/src/app/location/page.tsx
+++ apps/web/src/app/location/page.tsx
@@ -1,6 +1,11 @@
-import { LocationPage } from "../../features/location/LocationPage";
+import Navbar from '@/components/Navbar'
+import { LocationPage } from '../../features/location/LocationPage'
 
 export default function Page() {
-  return <LocationPage />;
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <Navbar />
+      <LocationPage />
+    </div>
+  )
 }
-
